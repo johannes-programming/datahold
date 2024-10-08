6b466b0922be13354c35fb91425d750e9bd92b16
Johannes <johannes-programming@mailfence.com>
2024-10-08 23:59:56 +0200

    u

---
+++ src/datahold/tests/__init__.py
@@ -0,0 +1,8 @@
+import unittest
+__all__ = ["test"]
+def test():
+    loader = unittest.TestLoader()
+    tests = loader.discover(start_dir="datahold.tests")
+    runner = unittest.TextTestRunner()
+    result = runner.run(tests)
+    return result
