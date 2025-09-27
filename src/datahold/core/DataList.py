b4ac0f210721c0ea1e3b38161ea6a110952e30a6
Johannes <johannes-programming@mailfence.com>
2025-09-28 00:15:36 +0200

    Version 2.0.4

--- src/datahold/core/DataList.py
+++ src/datahold/core/DataList.py
@@ -2,43 +2,14 @@ import collections
 from typing import *
 
 from datahold._utils import deco
+from datahold._utils.Cfg import Cfg
 from datahold.core.DataABC import DataABC
 
 __all__ = ["DataList"]
 
 
 @deco.funcDeco(
-    "__add__",
-    "__contains__",
-    "__delitem__",
-    "__eq__",
-    "__format__",
-    "__ge__",
-    "__getitem__",
-    "__gt__",
-    "__iadd__",
-    "__imul__",
-    "__iter__",
-    "__le__",
-    "__len__",
-    "__lt__",
-    "__mul__",
-    "__repr__",
-    "__reversed__",
-    "__rmul__",
-    "__setitem__",
-    "__str__",
-    "append",
-    "clear",
-    "copy",
-    "count",
-    "extend",
-    "index",
-    "insert",
-    "pop",
-    "remove",
-    "reverse",
-    "sort",
+    *Cfg.cfg.data["datafuncs"]["List"],
     Frozen=tuple,
     NonFrozen=list,
 )
