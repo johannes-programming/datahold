9cbcf3674f8511227883305742f6187b6a18141c
Johannes <johannes.programming@gmail.com>
2025-11-18 12:50:07 +0100

    Version 2.1.0

--- src/datahold/core/DataDict.py
+++ src/datahold/core/DataDict.py
@@ -7,16 +7,19 @@ from datahold._utils import deco
 from datahold._utils.Cfg import Cfg
 from datahold.core.DataABC import DataABC
 
+Key = TypeVar("Key")
+Value = TypeVar("Value")
+
 
 @deco.funcDeco(
     funcnames=Cfg.cfg.data["datafuncs"]["Dict"],
-    Frozen=frozendict,
-    NonFrozen=dict,
+    Frozen=frozendict[Key, Value],
+    NonFrozen=dict[Key, Value],
 )
 @deco.initDeco(
-    Frozen=frozendict,
-    NonFrozen=dict,
+    Frozen=frozendict[Key, Value],
+    NonFrozen=dict[Key, Value],
 )
-class DataDict(DataABC, collections.abc.MutableMapping):
+class DataDict(DataABC, collections.abc.MutableMapping[Key, Value]):
     __slots__ = ()
-    data: frozendict
+    data: frozendict[Key, Value]
