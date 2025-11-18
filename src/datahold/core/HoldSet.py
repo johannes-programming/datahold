0eb41d2b0d1eb696f8cd3f6693ca56980b68bd37
Johannes <johannes.programming@gmail.com>
2025-11-18 12:50:07 +0100

    Version 2.1.0

--- src/datahold/core/HoldSet.py
+++ src/datahold/core/HoldSet.py
@@ -9,7 +9,10 @@ __all__ = [
 ]
 
 
+Item = TypeVar("Item")
+
+
 @deco.dataDeco()
-class HoldSet(DataSet, HoldABC):
+class HoldSet(HoldABC, DataSet[Item]):
     __slots__ = ()
-    data: frozenset
+    data: frozenset[Item]
