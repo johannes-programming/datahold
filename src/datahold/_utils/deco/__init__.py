88685a8faa7c3bfae80cc11c94bf672f08056f92
Johannes <johannes.programming@gmail.com>
2025-11-22 23:13:55 +0100

    Version 2.2.1

--- src/datahold/_utils/deco/__init__.py
+++ src/datahold/_utils/deco/__init__.py
@@ -1,3 +1,4 @@
 from datahold._utils.deco.dataDeco import dataDeco
-from datahold._utils.deco.funcDeco import funcDeco
+from datahold._utils.deco.frozenDeco import frozenDeco
 from datahold._utils.deco.initDeco import initDeco
+from datahold._utils.deco.unfrozenDeco import unfrozenDeco
