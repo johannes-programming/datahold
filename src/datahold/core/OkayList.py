2768da479a57665f19e2329ee640a6db17fc988b
Johannes <johannes-programming@mailfence.com>
2025-12-10 10:51:17 +0100

    Version 2.3.0a1

--- src/datahold/core/OkayList.py
+++ src/datahold/core/OkayList.py
@@ -3,24 +3,20 @@ from typing import *
 import setdoc
 
 from datahold.core.HoldList import HoldList
-from datahold.core.OkayABC import OkayABC
+from datahold.core.OkayObject import OkayObject
 
 __all__ = ["OkayList"]
 
 Item = TypeVar("Item")
 
 
-class OkayList(OkayABC, HoldList[Item]):
-    __slots__ = ()
+class OkayList(OkayObject, HoldList[Item]):
     data: tuple[Item, ...]
+    __slots__ = ()
 
     @setdoc.basic
     def __add__(self: Self, other: Any, /) -> Self:
         return type(self)(self._data + list(other))
-
-    @setdoc.basic
-    def __init__(self: Self, data: Iterable = ()) -> None:
-        self.data = data
 
     @setdoc.basic
     def __mul__(self: Self, value: SupportsIndex, /) -> Self:
@@ -29,11 +25,3 @@ class OkayList(OkayABC, HoldList[Item]):
     @setdoc.basic
     def __rmul__(self: Self, value: SupportsIndex, /) -> Self:
         return self * value
-
-    def count(self: Self, value: Any, /) -> int:
-        "This method returns the number of occurences of value."
-        return self._data.count(value)
-
-    def index(self: Self, /, *args: Any) -> int:
-        "This method returns the index of the first occurence of value, or raises a ValueError if value is not present."
-        return self._data.index(*args)
