8b360bb851cadf739be73e46cf45d63b6db5f621
Johannes <johannes-programming@mailfence.com>
2025-12-10 10:51:17 +0100

    Version 2.3.0a1

--- src/datahold/_utils/deco/__init__.py
+++
@@ -1,4 +0,0 @@
-from datahold._utils.deco.dataDeco import dataDeco
-from datahold._utils.deco.frozenDeco import frozenDeco
-from datahold._utils.deco.initDeco import initDeco
-from datahold._utils.deco.unfrozenDeco import unfrozenDeco
