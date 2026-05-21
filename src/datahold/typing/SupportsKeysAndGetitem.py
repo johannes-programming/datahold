845724d5c174d3b784730743ab64dd0c8eebe684
Johannes <johannes-programming@mailfence.com>
2026-05-21 12:44:41 +0200

    typing

---
+++ src/datahold/typing/SupportsKeysAndGetitem.py
@@ -0,0 +1,13 @@
+from collections.abc import Iterable
+from typing import Protocol, TypeVar, Any
+
+Key = TypeVar("Key", covariant=True)
+Value = TypeVar("Value", covariant=True)
+
+__all__ = ["SupportsKeysAndGetitem"]
+class SupportsKeysAndGetitem(Protocol[Key, Value]):
+    def keys(self) -> Iterable[Key]:
+        ...
+
+    def __getitem__(self, key: Any, /) -> Value:
+        ...
