8a85d28140955daac90d5b4220c12e3de6fc3caa
Johannes <johannes-programming@mailfence.com>
2026-05-28 13:05:03 +0200

    tested

--- src/datahold/base/BaseDataSet.py
+++ src/datahold/base/BaseDataSet.py
@@ -1,6 +1,7 @@
 from abc import abstractmethod
 from collections.abc import Set as AbstractSet
-from typing import Any, Generic, Self, TypeVar
+from collections.abc import Iterable, Iterator
+from typing import Any, Generic, Self, TypeVar, overload, cast
 
 import setdoc
 
@@ -9,90 +10,164 @@ from .BaseDataObject import BaseDataObject
 __all__ = ["BaseDataSet"]
 
 Item = TypeVar("Item", covariant=True)
+Item_ = TypeVar("Item_")
 
 
 class BaseDataSet(BaseDataObject, Generic[Item]):
     __slots__ = ()
 
-    @setdoc.setdoc(set.__and__.__doc__)
+    @overload
+    @setdoc.basic
+    def __and__(self: Self, other: frozenset[object], /) -> set[Item]:...
+
+    @overload
+    @setdoc.basic
+    def __and__(self: Self, other: set[object], /) -> set[Item]:...
+
+
+    @setdoc.basic
     def __and__(
         self: Self,
-        value: Any,
+        other: frozenset[object] | set[object],
         /,
-    ) -> Any:
-        return set(self.data).__and__(value)
+    ) -> set[Item]:
+        return set(self.data).__and__(other)
 
-    @setdoc.setdoc(set.__contains__.__doc__)
-    def __contains__(self: Self, value: Any, /) -> Any:
-        return set(self.data).__contains__(value)
+    @setdoc.basic
+    def __contains__(self: Self, other: object, /) -> bool:
+        return set(self.data).__contains__(other)
 
     @abstractmethod
-    @setdoc.setdoc(set.__init__.__doc__)
-    def __init__(self: Self, data: Any = (), /) -> None: ...
+    @setdoc.basic
+    def __init__(self: Self, data: Iterable[Item_] = (), /) -> None: ...
 
-    @setdoc.setdoc(set.__iter__.__doc__)
-    def __iter__(self: Self, /) -> Any:
+    @setdoc.basic
+    def __iter__(self: Self, /) -> Iterator[Item]:
         return set(self.data).__iter__()
 
-    @setdoc.setdoc(set.__len__.__doc__)
-    def __len__(self: Self, /) -> Any:
+    @setdoc.basic
+    def __len__(self: Self, /) -> int:
         return set(self.data).__len__()
 
-    @setdoc.setdoc(set.__or__.__doc__)
+    @overload
+    @setdoc.basic
+    def __or__(self: Self, other: frozenset[Item_], /) -> set[Item|Item_]:...
+
+    @overload
+    @setdoc.basic
+    def __or__(self: Self, other: set[Item_], /) -> set[Item|Item_]:...
+    
+    @setdoc.basic
     def __or__(
         self: Self,
-        value: Any,
+        other: Any,
         /,
     ) -> Any:
-        return set(self.data).__or__(value)
-
-    @setdoc.setdoc(set.__rand__.__doc__)  # type: ignore[attr-defined]
+        return set(self.data).__or__(other)
+    
+    @overload
+    @setdoc.basic 
+    def __rand__(
+        self: Self, other: frozenset[object], /
+    ) -> frozenset[Item]:...
+    
+    @overload
+    @setdoc.basic 
+    def __rand__(
+        self: Self, other: set[object], /
+    ) -> set[Item]:...
+    
+    @setdoc.basic 
     def __rand__(
         self: Self,
-        value: Any,
+        other: Any,
         /,
     ) -> Any:
-        return set(self.data).__rand__(value)  # type: ignore[attr-defined]
+        return set(self.data).__rand__(other)  # type: ignore[attr-defined]
 
     @setdoc.setdoc(set.__repr__.__doc__)
     def __repr__(self: Self, /) -> str:
         return set(self.data).__repr__()
 
-    @setdoc.setdoc(set.__ror__.__doc__)
+    @overload
+    @setdoc.basic
     def __ror__(
         self: Self,
-        value: Any,
+        other: frozenset[Item_],
+        /,
+    ) -> frozenset[Item|Item_]: ...
+
+    @overload
+    @setdoc.basic
+    def __ror__(
+        self: Self,
+        other: set[Item_],
+        /,
+    ) -> set[Item|Item_]:...
+
+    @setdoc.basic
+    def __ror__(
+        self: Self,
+        other: Any,
         /,
     ) -> Any:
-        return set(self.data).__ror__(value)  # type: ignore[attr-defined]
+        return set(self.data).__ror__(other)  # type: ignore[attr-defined]
 
-    @setdoc.setdoc(set.__rsub__.__doc__)  # type: ignore[attr-defined]
+    @overload
+    @setdoc.basic
+    def __rsub__(
+        self: Self, 
+        other: frozenset[Item_],
+        /,
+    ) -> frozenset[Item_]:
+        ...
+
+    @overload
+    @setdoc.basic
     def __rsub__(
         self: Self,
-        value: Any,
+        other: set[Item_],
+        /,
+    ) -> set[Item_]:
+        ...
+
+    @setdoc.basic 
+    def __rsub__(
+        self: Self,
+        other: Any,
         /,
     ) -> Any:
-        return set(self.data).__rsub__(value)  # type: ignore[attr-defined]
+        return set(self.data).__rsub__(other)  # type: ignore[attr-defined]
 
-    @setdoc.setdoc(set.__rxor__.__doc__)  # type: ignore[attr-defined]
-    def __rxor__(self: Self, value: Any, /) -> Any:
-        return set(self.data).__rxor__(value)  # type: ignore[attr-defined]
+    @overload
+    @setdoc.basic 
+    def __rxor__(self: Self, other: frozenset[Item_], /) -> frozenset[Item|Item_]:
+        ...
 
-    @setdoc.setdoc(set.__sub__.__doc__)
+    @overload
+    @setdoc.basic 
+    def __rxor__(self: Self, other: set[Item_], /) -> set[Item|Item_]:
+        ...
+
+    @setdoc.basic 
+    def __rxor__(self: Self, other: Any, /) -> Any:
+        return set(self.data).__rxor__(other)# type: ignore[attr-defined] 
+
+    @setdoc.basic 
     def __sub__(
         self: Self,
-        value: Any,
+        other: frozenset[object]|set[object],
         /,
-    ) -> Any:
-        return set(self.data).__sub__(value)
+    ) -> set[Item]:
+        return set(self.data).__sub__(other)
 
-    @setdoc.setdoc(set.__xor__.__doc__)
+    @setdoc.basic
     def __xor__(
         self: Self,
-        value: Any,
+        other: frozenset[Item_]|set[Item_],
         /,
-    ) -> Any:
-        return set(self.data).__xor__(value)
+    ) -> set[Item|Item_]:
+        return set(self.data).__xor__(other)
 
     @setdoc.setdoc(set.difference.__doc__)
     def difference(self: Self, /, *others: Any) -> Any:
