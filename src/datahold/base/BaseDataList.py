06f9db4b0b7c70a54dd9f82bd88939b0b2a6c918
Johannes <johannes-programming@mailfence.com>
2026-05-28 11:55:01 +0200

    tested

--- src/datahold/base/BaseDataList.py
+++ src/datahold/base/BaseDataList.py
@@ -16,45 +16,45 @@ Item_ = TypeVar("Item_")
 class BaseDataList(BaseDataObject, Generic[Item]):
     __slots__ = ()
 
-    @setdoc.setdoc(list.__add__.__doc__)
-    def __add__(self: Self, value: list[Item_], /) -> list[Item | Item_]:
-        return list(self.data).__add__(value)
+    @setdoc.basic
+    def __add__(self: Self, other: list[Item_], /) -> list[Item | Item_]:
+        return list(self.data).__add__(other)
 
-    @setdoc.setdoc(list.__contains__.__doc__)
-    def __contains__(self: Self, value: object, /) -> bool:
-        return list(self.data).__contains__(value)
+    @setdoc.basic
+    def __contains__(self: Self, other: object, /) -> bool:
+        return list(self.data).__contains__(other)
 
-    @setdoc.setdoc(list.__getitem__.__doc__)
-    def __getitem__(self: Self, key: SupportsIndex, /) -> Item:
-        return list(self.data).__getitem__(key)
+    @setdoc.basic
+    def __getitem__(self: Self, index: SupportsIndex, /) -> Item:
+        return list(self.data).__getitem__(index)
 
     @abstractmethod
-    @setdoc.setdoc(list.__init__.__doc__)
+    @setdoc.basic
     def __init__(self: Self, data: Iterable[Item] = (), /) -> None: ...
 
-    @setdoc.setdoc(list.__iter__.__doc__)
+    @setdoc.basic
     def __iter__(self: Self, /) -> Iterator[Item]:
         return list(self.data).__iter__()
 
-    @setdoc.setdoc(list.__len__.__doc__)
+    @setdoc.basic
     def __len__(self: Self, /) -> int:
         return list(self.data).__len__()
 
-    @setdoc.setdoc(list.__mul__.__doc__)
-    def __mul__(self: Self, value: SupportsIndex, /) -> list[Item]:
-        return list(self.data).__mul__(value)
+    @setdoc.basic
+    def __mul__(self: Self, other: SupportsIndex, /) -> list[Item]:
+        return list(self.data).__mul__(other)
 
-    @setdoc.setdoc(list.__repr__.__doc__)
+    @setdoc.basic
     def __repr__(self: Self, /) -> str:
         return list(self.data).__repr__()
 
-    @setdoc.setdoc(list.__reversed__.__doc__)
+    @setdoc.basic
     def __reversed__(self: Self, /) -> Iterator[Item]:
         return list(self.data).__reversed__()
 
-    @setdoc.setdoc(list.__rmul__.__doc__)
-    def __rmul__(self: Self, value: SupportsIndex, /) -> list[Item]:
-        return list(self.data).__rmul__(value)
+    @setdoc.basic
+    def __rmul__(self: Self, other: SupportsIndex, /) -> list[Item]:
+        return list(self.data).__rmul__(other)
 
     @setdoc.setdoc(list.count.__doc__)
     def count(self: Self, value: object, /) -> int:
