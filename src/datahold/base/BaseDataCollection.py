e7b50dc4a2b6409eccf2fc623e804441fda6098b
Johannes <johannes.programming@gmail.com>
2026-07-07 19:27:56 +0200

    x
    x
    
    x
    
    x
    
    x
    
    Version 3.0.49+local

--- src/datahold/base/BaseDataCollection.py
+++ src/datahold/base/BaseDataCollection.py
@@ -1,29 +1,35 @@
 """Provide BaseDataCollection."""
 
+from __future__ import annotations
+
 __all__ = ["BaseDataCollection"]
 
 from abc import abstractmethod
-from collections.abc import Collection, Hashable, Iterator
-from typing import Self, TypeVar
+from collections.abc import Collection, Hashable
+from typing import Final, Protocol, Self, TypeVar
 
 import setdoc
 
-from ..typing.HashableCollection import HashableCollection
-from .BaseDataObject import BaseDataObject
+from .BaseDataIterable import BaseDataIterable
 
 Item = TypeVar("Item", covariant=True)
 
 
-class BaseDataCollection(BaseDataObject, Collection[Item]):
+class Data(Collection[Item], Protocol[Item]):
+    """Provide hashable collection protocol."""
+
+    @setdoc.basic
+    def __hash__(self: Self) -> int: ...
+
+
+class BaseDataCollection(BaseDataIterable[Item], Collection[Item]):
     __slots__ = ()
+
+    Data: Final[type[Data]] = Data  # type: ignore[type-abstract, type-arg]
 
     @setdoc.basic
     def __contains__(self: Self, other: Hashable, /) -> bool:
-        return other in self.data
-
-    @setdoc.basic
-    def __iter__(self: Self, /) -> Iterator[Item]:
-        return iter(self.data)
+        return other in self.data  # type: ignore[attr-defined]
 
     @setdoc.basic
     def __len__(self: Self, /) -> int:
@@ -31,4 +37,6 @@ class BaseDataCollection(BaseDataObject, Collection[Item]):
 
     @property
     @abstractmethod
-    def data(self: Self) -> HashableCollection[Item]: ...
+    @setdoc.basic
+    def data(self: Self) -> Data[Item]:  # type: ignore[valid-type]
+        ...
