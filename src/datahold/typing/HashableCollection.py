4029610482ace261a4cdc4cd361d5f9f46ff2312
Johannes <johannes.programming@gmail.com>
2026-07-07 18:15:10 +0200

    x

--- src/datahold/typing/HashableCollection.py
+++
@@ -1,8 +0,0 @@
-"""Provide HashableCollection."""
-
-__all__: list[str] = ["HashableCollection"]
-from typing import Final
-
-from datahold.base.BaseDataCollection import BaseDataCollection
-
-HashableCollection: Final[type[BaseDataCollection.Data]] = BaseDataCollection.Data  # type: ignore[type-abstract, type-arg]
