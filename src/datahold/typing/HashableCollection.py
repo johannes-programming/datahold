9e6a7f720ca1b73b3b05b015912495dedafc4691
Johannes <johannes.programming@gmail.com>
2026-07-07 11:39:48 +0200

    Version 3.0.49+local

--- src/datahold/typing/HashableCollection.py
+++ src/datahold/typing/HashableCollection.py
@@ -3,19 +3,13 @@
 __all__: list[str] = ["HashableCollection"]
 
 from collections.abc import Collection
-from collections.abc import Container as Container_
-from collections.abc import Hashable
-from typing import Protocol, Self, TypeVar
+from typing import Protocol, TypeVar
 
-import setdoc
-
-
-class Container(Container_[Hashable], Protocol): ...
-
+from .HashableContainer import HashableContainer
 
 Item = TypeVar("Item", covariant=True)
 
 
-class HashableCollection(Container, Collection[Item], Protocol[Item]):
-    @setdoc.basic
-    def __hash__(self: Self) -> int: ...
+class HashableCollection(
+    HashableContainer, Collection[Item], Protocol[Item]
+): ...
