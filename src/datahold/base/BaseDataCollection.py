05f5bb4db8fd764be551ad20cf59e2b56a833849
Johannes <johannes.programming@gmail.com>
2026-07-11 18:59:57 +0200

    Version 3.0.56+local

--- src/datahold/base/BaseDataCollection.py
+++ src/datahold/base/BaseDataCollection.py
@@ -3,7 +3,7 @@
 __all__ = ["BaseDataCollection"]
 
 from abc import abstractmethod
-from collections.abc import Collection, Hashable, Iterator
+from collections.abc import Container, Iterable, Iterator, Sized
 from typing import Protocol, Self, TypeVar
 
 import setdoc
@@ -14,19 +14,33 @@ DataItem = TypeVar("DataItem", covariant=True)
 Item = TypeVar("Item", covariant=True)
 
 
-class BaseDataCollection(BaseDataObject, Collection[Item]):
+class BaseDataCollection(
+    BaseDataObject,
+    Sized,
+    Iterable[Item],
+    Container[object],
+):
     __slots__ = ()
 
     class Data(
-        Collection[DataItem],
+        Sized,
+        Iterable[DataItem],
+        Container[object],
         BaseDataObject.Data,
         Protocol[DataItem],
     ):
         """Provide collection data protocol."""
 
     @setdoc.basic
-    def __contains__(self: Self, other: Hashable, /) -> bool:
-        return other in self.data
+    def __contains__(self: Self, other: object, /) -> bool:
+        try:
+            return other in self.data
+        except TypeError:
+            pass
+        for x in self:
+            if x is other and x == other:
+                return True
+        return False
 
     @setdoc.basic
     def __iter__(self: Self, /) -> Iterator[Item]:
