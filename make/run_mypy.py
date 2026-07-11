fa8b4f4dc0043c877db071005f78c23f7662f14f
Johannes <johannes.programming@gmail.com>
2026-07-11 07:14:05 +0200

    Revert "make"
    This reverts commit 5ae88ac5b0f4f9cd7e6237bc38b45a1cdd7f6542.

--- make/run_mypy.py
+++ make/run_mypy.py
@@ -3,4 +3,8 @@ from collections.abc import Sequence
 from typing import reveal_type
 
 from datahold import BaseDataList
+from datahold.base.BaseDataReversible import BaseDataReversible
 
+reveal_type(BaseDataReversible.__reversed__)
+reveal_type(Sequence.__reversed__)
+reveal_type(BaseDataList.__reversed__)
