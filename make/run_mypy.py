8b412ed7f2c021332982db4d1546bef4162fd879
Johannes <johannes.programming@gmail.com>
2026-07-11 07:13:59 +0200

    make

--- make/run_mypy.py
+++ make/run_mypy.py
@@ -3,8 +3,4 @@ from collections.abc import Sequence
 from typing import reveal_type
 
 from datahold import BaseDataList
-from datahold.base.BaseDataReversible import BaseDataReversible
 
-reveal_type(BaseDataReversible.__reversed__)
-reveal_type(Sequence.__reversed__)
-reveal_type(BaseDataList.__reversed__)
