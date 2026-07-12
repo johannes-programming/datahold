c2aaeb7b1bc061cb671f89de21f816eb11e41605
Johannes <johannes.programming@gmail.com>
2026-07-12 17:36:01 +0200

    x

---
+++ src/datahold/base/BaseDataMapping.py
@@ -0,0 +1,63 @@
+from __future__ import annotations
+
+__all__: list[str] = ["BaseDataMapping"]
+from abc import abstractmethod
+from collections.abc import (
+    Container,
+    Hashable,
+    Iterable,
+    Iterator,
+    Mapping,
+    Sized,
+)
+from typing import Protocol, Self, TypeVar
+
+import setdoc
+
+DataKey = TypeVar("DataKey", covariant=True)
+DataValue = TypeVar("DataValue", covariant=True)
+Key = TypeVar("Key", covariant=True)
+Value = TypeVar("Value", covariant=True)
+
+
+class Container_(Container[Hashable]): ...
+
+
+class BaseDataMapping(  # type: ignore[type-var]
+    Container_,
+    Mapping[Key, Value],
+):
+    """Act as a base class for concrete mapping implementations backed by a data attribute."""
+
+    # this abc exists to provide the easiest possible template for a Mapping
+    # a subclass must only override ``data``
+    # and it is immediately non-abstract
+
+    __slots__ = ()
+
+    class Data(
+        Hashable, Sized, Iterable[DataKey], Protocol[DataKey, DataValue]
+    ):
+        """Define the protocol that the data property must satisfy."""
+
+        @setdoc.basic
+        def __getitem__(self: Self, key: Hashable) -> DataValue: ...
+
+    __contains__ = Mapping[Hashable, Value].__contains__
+
+    @setdoc.basic
+    def __getitem__(self: Self, key: Hashable) -> Value:
+        return self.data[key]
+
+    @setdoc.basic
+    def __iter__(self: Self) -> Iterator[Key]:
+        return iter(self.data)
+
+    @setdoc.basic
+    def __len__(self: Self) -> int:
+        return len(self.data)
+
+    @property
+    @abstractmethod
+    @setdoc.basic
+    def data(self: Self) -> BaseDataMapping.Data[Key, Value]: ...
