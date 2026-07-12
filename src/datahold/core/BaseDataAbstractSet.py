70ebaea5ee7df9399c07a6abbb93f41045afd89c
Johannes <johannes.programming@gmail.com>
2026-07-12 16:12:25 +0200

    x

---
+++ src/datahold/core/BaseDataAbstractSet.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+__all__: list[str] = ["BaseDataAbstractSet"]
+from abc import abstractmethod
+from collections.abc import Container, Hashable, Iterable, Iterator, Set, Sized
+from typing import Never, Protocol, Self, TypeVar
+
+import setdoc
+
+DataItem = TypeVar("DataItem", covariant=True)
+Item = TypeVar("Item", covariant=True)
+
+
+class Container_(Container[object]): ...
+
+
+class BaseDataAbstractSet(Container_, Set[Item]):
+    """Act as a base class for concrete set implementations backed by a data attribute."""
+
+    __slots__ = ()
+
+    class Data(
+        Hashable,
+        Sized,
+        Iterable[DataItem],
+        Container[Never],  # every TypeError is worked around
+        Protocol[DataItem],
+    ):
+        """Define the protocol that the data property must satisfy."""
+
+    @setdoc.basic
+    def __contains__(self: Self, other: object) -> bool:
+        try:
+            return other in self.data
+        except TypeError:
+            return False
+
+    @setdoc.basic
+    def __iter__(self: Self) -> Iterator[Item]:
+        return iter(self.data)
+
+    @setdoc.basic
+    def __len__(self: Self) -> int:
+        return len(self.data)
+
+    @property
+    @abstractmethod
+    @setdoc.basic
+    def data(self: Self) -> BaseDataAbstractSet.Data[Item]: ...
