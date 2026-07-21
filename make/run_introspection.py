e1619609c9554427c0de42ad0450ef91f94590fe
Johannes <johannes.programming@gmail.com>
2026-07-21 21:03:20 +0200

    make

--- make/run_introspection.py
+++ make/run_introspection.py
@@ -1,10 +1,17 @@
 __all__: list[str] = []
 
-from datahold import FrozenHoldList
+from datahold import FrozenHoldList, BaseDataList
+from typing import Any
+
+def search_mro(cls_:type[Any], /, name:str) -> None:
+    print("find %r from %s" % (name, cls_))
+    for cls in cls_.__mro__:
+        if name in cls.__dict__:
+            print(f"{cls.__name__}: {cls.__dict__[name]!r}")
+    print()
 
 print("FrozenHoldList.__mro__", FrozenHoldList.__mro__)
 print("FrozenHoldList.__hash__", FrozenHoldList.__hash__)
 
-for cls in FrozenHoldList.__mro__:
-    if "__hash__" in cls.__dict__:
-        print(f"{cls.__name__}: {cls.__dict__['__hash__']!r}")
+search_mro(FrozenHoldList, "__hash__")
+search_mro(BaseDataList, "__hash__")
