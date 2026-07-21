203880e8e2c73fd5a9369f4e1b2e7f196fbe45a3
Johannes <johannes.programming@gmail.com>
2026-07-21 20:55:17 +0200

    make

--- make/run_introspection.py
+++ make/run_introspection.py
@@ -1,8 +1,10 @@
 __all__: list[str] = []
-from collections.abc import Sequence
-from typing import reveal_type
 
 from datahold import FrozenHoldList
 
 print("FrozenHoldList.__mro__", FrozenHoldList.__mro__)
 print("FrozenHoldList.__hash__", FrozenHoldList.__hash__)
+
+for cls in FrozenHoldList.__mro__:
+    if "__hash__" in cls.__dict__:
+        print(f"{cls.__name__}: {cls.__dict__['__hash__']!r}")
