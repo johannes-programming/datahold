b46a02e053805a2f9931709d9bdbdcff7632e58b
Johannes <johannes.programming@gmail.com>
2026-07-21 20:39:20 +0200

    make

--- make/run_introspection.py
+++ make/run_introspection.py
@@ -3,3 +3,6 @@ from collections.abc import Sequence
 from typing import reveal_type
 
 from datahold import FrozenHoldList
+
+print("FrozenHoldList.__mro__", FrozenHoldList.__mro__)
+print("FrozenHoldList.__hash__", FrozenHoldList.__hash__)
