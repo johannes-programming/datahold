3c91b112c58356d31ab61dbda47a5ff36ab276cd
Johannes <johannes.programming@gmail.com>
2026-07-22 00:51:09 +0200

    x

--- src/datahold/base/BaseHoldCollection.py
+++ src/datahold/base/BaseHoldCollection.py
@@ -6,4 +6,6 @@ from .BaseDataCollection import BaseDataCollection
 
 
 class BaseHoldCollection[Item](BaseDataCollection[Item]):
+    """Provide abc for usable classes with slots."""
+
     __slots__ = ("_data",)
