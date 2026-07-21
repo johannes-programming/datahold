d5bedd43210004a511ce9ec30f5333b3cfe8302c
Johannes <johannes.programming@gmail.com>
2026-07-21 16:41:59 +0200

    x

---
+++ src/datahold/base/BaseDataAbstractSet.py
@@ -0,0 +1,15 @@
+"""Provide BaseDataAbstractSet."""
+
+__all__ = ["BaseDataAbstractSet"]
+
+
+from collections.abc import Set
+
+from .BaseDataCollection import BaseDataCollection
+
+
+class BaseDataAbstractSet[Item](
+    BaseDataCollection[Item], 
+    Set[Item],
+):
+    __slots__ = ()
--- src/datahold/base/BaseDataCollection.py
+++ src/datahold/base/BaseDataCollection.py
@@ -2,31 +2,93 @@
 
 __all__ = ["BaseDataCollection"]
 
-from abc import abstractmethod
-from collections.abc import Collection, Hashable, Iterator
-from typing import Protocol, Self, TypeVar
+
+from collections.abc import Collection, Hashable, Iterator, Container, Sized, Iterable
+from typing import Protocol, Self, Any, Never
 
 import setdoc
 
-from .BaseDataObject import BaseDataObject
-
-DataItem = TypeVar("DataItem", covariant=True)
-Item = TypeVar("Item", covariant=True)
+from abc import ABCMeta, abstractmethod
 
 
-class BaseDataCollection(BaseDataObject, Collection[Item]):
+class BaseDataCollection[Item](
+    Collection[Item], 
+    metaclass=ABCMeta,
+):
     __slots__ = ()
 
-    class Data(
-        Collection[DataItem],
-        BaseDataObject.Data,
+    class Data[DataItem](
+        Sized,
+        Iterable[DataItem],
+        Container[Never],
+        Hashable,
         Protocol[DataItem],
     ):
         """Provide collection data protocol."""
 
+
     @setdoc.basic
-    def __contains__(self: Self, other: Hashable, /) -> bool:
-        return other in self.data
+    def __eq__(self: Self, other: Any, /) -> Any:
+        if isinstance(other, BaseDataCollection):
+            return self.data.__eq__(other.data)
+        else:
+            return NotImplemented
+
+    #@setdoc.basic
+    #def __ge__(self: Self, other: Any, /) -> Any:
+    #    if not isinstance(other, BaseDataCollection):
+    #        return NotImplemented
+    #    try:
+    #        return self.data.__ge__(other.data)  # type: ignore[operator]
+    #    except Exception:
+    #        return NotImplemented
+
+    #@setdoc.basic
+    #def __gt__(self: Self, other: Any, /) -> Any:
+    #    if not isinstance(other, BaseDataCollection):
+    #        return NotImplemented
+    #    try:
+    #        return self.data.__gt__(other.data)  # type: ignore[operator]
+    #    except Exception:
+    #        return NotImplemented
+
+    #@abstractmethod
+    #@setdoc.basic
+    #def __init__(self: Self, data: Self, /) -> None: ...
+
+    #@setdoc.basic
+    #def __le__(self: Self, other: Any, /) -> Any:
+    #    if not isinstance(other, BaseDataCollection):
+    #        return NotImplemented
+    #    try:
+    #        return self.data.__le__(other.data)  # type: ignore[operator]
+    #    except Exception:
+    #        return NotImplemented
+
+    #@setdoc.basic
+    #def __lt__(self: Self, other: Any, /) -> Any:
+    #    if not isinstance(other, BaseDataCollection):
+    #        return NotImplemented
+    #    try:
+    #        return self.data.__lt__(other.data)  # type: ignore[operator]
+    #    except Exception:
+    #        return NotImplemented
+
+    #@setdoc.basic
+    #def __ne__(self: Self, other: Any, /) -> Any:
+    #    if isinstance(other, BaseDataCollection):
+    #        return self.data.__ne__(other.data)
+    #    else:
+    #        return NotImplemented
+
+
+
+    @setdoc.basic
+    def __contains__(self: Self, other: object, /) -> bool:
+        try:
+            return other in self.data
+        except TypeError:
+            return other in (x for x in self.data)
 
     @setdoc.basic
     def __iter__(self: Self, /) -> Iterator[Item]:
