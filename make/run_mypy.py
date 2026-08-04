97c03d8cb53e8d620d9481388979a58cd0d32420
Johannes <johannes.programming@gmail.com>
2026-08-04 16:43:38 +0200

    make

--- make/run_mypy.py
+++ make/run_mypy.py
@@ -1,5 +1,9 @@
 __all__: list[str] = []
 from typing import reveal_type
 
+import datahold
+
 reveal_type(list.__ge__)
 reveal_type(list.sort)
+reveal_type(datahold.MutableSequence.__frozen__)
+reveal_type(datahold.MutableListLike.__frozen__)
