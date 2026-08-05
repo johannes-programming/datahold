5b4eeca37195f0ab482c4899063a10115ac6ef0b
Johannes <johannes.programming@gmail.com>
2026-08-05 20:14:51 +0200

    tested

---
+++ tests/test_metaclass.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+__all__: list[str] = ["TestMetaclass"]
+
+import unittest
+from abc import ABCMeta
+from typing import Any, Self
+
+from Lazy import Lazy
+
+import datahold
+
+
+class TestMetaclass(unittest.TestCase):
+
+    def test_metaclass(self: Self, /) -> None:
+        datatype: type[Any]
+        for name in Lazy.lazy.datatypes.keys():
+            with self.subTest(datatype=name):
+                datatype = getattr(datahold, name)
+                self.assertIs(type(datatype), ABCMeta)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
