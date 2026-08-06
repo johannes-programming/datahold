1480053206188da3af612a958551656c39b9fdb2
Johannes <johannes.programming@gmail.com>
2026-08-06 11:20:18 +0200

    make

--- make/run_mypy.py
+++ make/run_mypy.py
@@ -1,5 +1,8 @@
 __all__: list[str] = []
+from collections import abc
 from typing import reveal_type
+
+from frozendict import frozendict
 
 import datahold
 
@@ -7,3 +10,9 @@ reveal_type(list.__ge__)
 reveal_type(list.sort)
 reveal_type(datahold.MutableSequence.__frozen__)
 reveal_type(datahold.MutableListLike.__frozen__)
+
+
+def cast_as_Mapping_Frozen[Key: abc.Hashable, Value](
+    value: frozendict[Key | str, Value | None],
+) -> datahold.Mapping_Frozen[Key | str, Value | None]:
+    return value
